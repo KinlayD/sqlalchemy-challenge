e7fa6bdc9e64ee84e09532f24a1ff4b5dcd7fae5
KinlayDenning <89169906+KinlayD@users.noreply.github.com>
2022-07-20 16:18:59 +1000

    added to .ipynb and .py

--- app.py
+++ app.py
@@ -9,9 +9,7 @@ from collections import defaultdict
 from flask import Flask, jsonify
 
 
-#################################################
-# Database Setup
-#################################################
+# database setup
 database_path = "/Users/kinlaydenning/Documents/bootcamp2022/week_10_advanced_database_storage_and_retrieval/sqlalchemy-challenge/hawaii.sqlite"
 engine = create_engine(f"sqlite:///{database_path}")
 
@@ -22,23 +20,20 @@ base.prepare(engine, reflect=True)
 
 # Save reference to the table
 measurement = base.classes.measurement
+station = base.classes.station
 
-#################################################
-# Flask Setup
-#################################################
+# flask setup
 app = Flask(__name__)
 
-
-#################################################
-# Flask Routes
-#################################################
-
+# flask routes
 @app.route("/")
 def homepage():
 
     return (
         f"Available Routes:<br/>"
-        f"/api/v1.0/precipitation"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations"
+
     )
 
 @app.route("/api/v1.0/precipitation")
@@ -46,11 +41,22 @@ def precipitation():
     session = Session(engine)
     results = session.query(measurement.date, measurement.prcp).all()
     session.close()
-    d = {}
+
+    precipitation = {}
     for date, prcp in results:
         if type(prcp) == float:
-            d.setdefault(date, []).append(prcp)
-    return jsonify(d)
+            precipitation.setdefault(date, []).append(prcp)
+    return jsonify(precipitation)
+
+@app.route("/api/v1.0/stations")
+def stations():
+    session = Session(engine)
+    sq = session.query(measurement.station).\
+    filter(measurement.station == station.station).distinct().all()
+    session.close()
+    
+    sd_list = list(np.ravel(sq))
+    return jsonify(sd_list)
 
 if __name__ == '__main__':
     app.run(debug=True)
